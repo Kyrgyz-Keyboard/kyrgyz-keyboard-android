1ecad8bab54f0f123c3276d3700d5ccf99eb4470
Nikita Paniukhin <git@npanuhin.me>
2025-04-23 20:06:28 +0200

    Changed color of Enter back to blue

--- app/src/main/java/com/example/kyrgyz_keyboard_android/ui/theme/Color.kt
+++ app/src/main/java/com/example/kyrgyz_keyboard_android/ui/theme/Color.kt
@@ -2,6 +2,11 @@ package com.example.kyrgyz_keyboard_android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+
+val FlagRed = Color(0xffeb392a)
+val FlagYellow = Color(0xffffff59)
+val GBoardBlue = Color(0xFC1A73E8)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -13,12 +18,8 @@ val Pink40 = Color(0xFF7D5260)
 // Keyboard Colors
 val KeyboardGray = Color(0xFFE8EAED)
 val KeyGray = Color(0xFFCFD3D9)
-val EnterColor = Color(0xffeb392a)
+val EnterColor = GBoardBlue
 val EnterImgColor = Color.White
 val KeyTextColor = Color.Black
 val KeyBackgroundColor = Color.White
 val KeyLockedBackgroundColor = Color.LightGray
-
-val FlagRed = Color(0xffeb392a)
-val FlagYellow = Color(0xffffff59)
-val GBoardBlue = Color(0xFC1A73E8)
