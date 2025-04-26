803da62d0ba257c025e594cfe1a248e312675747
Nikita Paniukhin <git@npanuhin.me>
2025-04-27 00:14:44 +0200

    Added function to calculate word stem

--- app/src/main/java/com/example/kyrgyz_keyboard_android/keyboard/predictive_text/PredictiveEngine.kt
+++ app/src/main/java/com/example/kyrgyz_keyboard_android/keyboard/predictive_text/PredictiveEngine.kt
@@ -1,15 +1,52 @@
 package com.example.kyrgyz_keyboard_android.keyboard.predictive_text
 
-import org.apertium.lttoolbox.LTProc
-
+import org.apertium.lttoolbox.process.FSTProcessor
+import org.apertium.utils.IOUtils
 import java.io.StringReader
 
+class PredictiveTextEngineImpl : PredictiveTextEngine {
+
+    private val binaryKirData = "app/src/main/res/kir.automorf.bin"
+    private val fstp = FSTProcessor()
+
+    init {
+        fstp.load(IOUtils.openFileAsByteBuffer(binaryKirData), binaryKirData)
+        fstp.initAnalysis()
+        if (!fstp.valid()) {
+            throw RuntimeException("Validity test for FSTProcessor failed")
+        }
+    }
+
+    fun getWordStem(word: String): String {
+        val output = StringBuilder()
+        fstp.analysis(StringReader(word + '\n'), output)
+
+        return output.toString()
+            .removePrefix("^")
+            .removeSuffix("$")
+            .split("/")
+            .map { reading ->
+                reading.replace(" ", "").substringBefore("<")
+            }
+            .filter { base ->
+                !base.startsWith("*")
+            }
+            .minOrNull() ?: word
+    }
+
+    override fun getPredictions(currentText: String): List<WordPrediction> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getNextWordPredictions(previousWords: String): List<WordPrediction> {
+        TODO("Not yet implemented")
+    }
+}
+
 fun main() {
-    val binaryKirData = "app/src/main/res/kir.automorf.bin"
-    val input = StringReader("боюнча\n")
-    val output = StringBuilder()
+    // LTProc.doMain(arrayOf(binaryKirData), input, output)
 
-    LTProc.doMain(arrayOf(binaryKirData), input, output)
-
-    println(output)
+    val predictiveEngine = PredictiveTextEngineImpl()
+    println(predictiveEngine.getWordStem("боюнча"))
+    println(predictiveEngine.getWordStem("экрандарынын"))
 }
