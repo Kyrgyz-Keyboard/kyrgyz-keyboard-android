8dac3e1825116e3833218ba8e28958694a1cc59b
Demi Sayaq <67751355+emikoo@users.noreply.github.com>
2025-10-07 14:34:09 +0200

    added extra letter

--- app/src/main/java/com/example/kyrgyz_keyboard_android/keyboard/model/CyrillicUiLayout.kt
+++ app/src/main/java/com/example/kyrgyz_keyboard_android/keyboard/model/CyrillicUiLayout.kt
@@ -24,6 +24,7 @@ object CyrillicUiLayout {
         KeyUiModel(ch = "н"),
         KeyUiModel(ch = "г"),
         KeyUiModel(ch = "ш"),
+        KeyUiModel(ch = "щ"),
         KeyUiModel(ch = "з"),
         KeyUiModel(ch = "х")
     )
